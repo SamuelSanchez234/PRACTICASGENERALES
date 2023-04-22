b54eda9d602929b089f5e88dc6cacd3bdda8bf93
Samuel Sanchez <130626550+SamuelSanchez234@users.noreply.github.com>
2023-04-22 08:48:18 -0600

    Update practica4-2.ino

--- practica4-2.ino
+++ practica4-2.ino
@@ -1,58 +1,54 @@
 #include <Servo.h>
 Servo servo1;
 int posicion_inicial = 100;
-int posicion1 = 45;
-int posicion2 = 90;
-int posicion3 = 135;
-int posicion4 = 180;
-int pulsador1 = 2;
-int pulsador2 = 3;
-int pulsador3 = 4;
-int pulsador4 = 5;
-int pulsador5 = 6;
-int estado1;
-int estado2;
-int estado3;
-int estado4;
-int estado5;
-
-
-
+int pos1 = 45;
+int poS2 = 90;
+int pos3 = 135;
+int pos4 = 180;
+int pul1 = 2;
+int pul2 = 3;
+int pul3 = 4;
+int puls4 = 5;
+int pul5 = 6;
+int lugar1;
+int lugar2;
+int lugar3;
+int lugar4;
+int lugar5;
 
 void setup() {
 Serial.begin(9600);
-pinMode(pulsador1,INPUT);
-pinMode(pulsador2,INPUT);
-pinMode(pulsador3,INPUT);
-pinMode(pulsador4,INPUT);
+pinMode(pul1,INPUT);
+pinMode(pul2,INPUT);
+pinMode(pul3,INPUT);
+pinMode(pul4,INPUT);
 servo1.attach(10);
 }
 void loop(){
-estado1 = digitalRead(pulsador1);
+estado1 = digitalRead(pul1);
  Serial.println(estado1);
-estado2 = digitalRead(pulsador2);
-estado3 = digitalRead(pulsador3);
-estado4 = digitalRead(pulsador4);
-estado5 = digitalRead(pulsador5);
-
-
-
-if(estado1 == 1){
- servo1.write(posicion1);
+estado2 = digitalRead(pul2);
+estado3 = digitalRead(pul3);
+estado4 = digitalRead(pul4);
+estado5 = digitalRead(pul5);
+ 
+if(lugar1 == 1){
+ servo1.write(pos1);
  }
-
-
-
- if(estado2 == 1){
- servo1.write(posicion2);
+ 
+ if(lugar2 == 1){
+ servo1.write(pos2);
  }
- if(estado3 == 1){
- servo1.write(posicion3);
+ 
+ if(lugar3 == 1){
+ servo1.write(pos3);
 }
-if(estado4 == 1){
- servo1.write(posicion4);
+ 
+if(lugar4 == 1){
+ servo1.write(pos4);
  }
- if(estado5 == 1){
+ 
+ if(lugar5 == 1){
  servo1.write(0);
 tone(8,15000);
 delay(1000);
